5e64efc4174efc59d56cbc0bd1fe592299180232
ivansouza <ivansouza@gmail.com>
2025-11-20 15:21:07 -0300

    Atualizar o service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,28 @@
 // Nome do cache e arquivos a serem armazenados em cache
 const CACHE_NAME = 'teleprompter-pro-cache-v1';
 const urlsToCache = [
-    './index.html', // Alterado para o novo nome do arquivo
-    // Ícones PWA (assumidos no diretório raiz para este exemplo)
+    './index.html', 
     '/icon-192.png',
     '/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
-    // Faz o Service Worker esperar até que o cache seja aberto
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(cache => {
                 console.log('Opened cache');
-                // Adiciona todos os recursos necessários ao cache
                 return cache.addAll(urlsToCache);
             })
     );
 });
 
 self.addEventListener('fetch', event => {
-    // Interceta pedidos de rede
     event.respondWith(
         caches.match(event.request)
             .then(response => {
-                // Retorna o recurso do cache, se encontrado
                 if (response) {
                     return response;
                 }
-                // Caso contrário, busca na rede
                 return fetch(event.request);
             })
     );
@@ -41,7 +35,6 @@ self.addEventListener('activate', event => {
             return Promise.all(
                 cacheNames.map(cacheName => {
                     if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        // Deleta caches antigos
                         return caches.delete(cacheName);
                     }
                 })
